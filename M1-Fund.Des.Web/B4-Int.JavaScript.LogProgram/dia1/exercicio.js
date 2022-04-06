358f461d80f891bd085365df889e1fbba33314cd
Renata Costa <rrenata_kelly@hotmail.com>
2022-04-06 18:54:51 -0300

    inicia bloco 4

---
+++ M1-Fund.Des.Web/B4-Int.JavaScript.LogProgram/dia1/exercicio.js
@@ -0,0 +1,9 @@
+const myName = "Renata";
+const birthCity = "Belo Horizonte"
+let birtthYear = 1988; 
+console.log (myName, birthCity, birtthYear);
+birtthYear = 2030;
+console.log (birtthYear);
+birthCity = "Governador Valadares";
+console.log (birthCity); // A mensagem de erro foi porque não se pode alterar o valor de uma constante. 
+//E birthCity é uma constante.
