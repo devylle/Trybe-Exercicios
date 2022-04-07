75bcc80f189059b8d235ca771e2372eb8132d0e3
Renata Costa <rrenata_kelly@hotmail.com>
2022-04-07 00:26:07 -0300

    1 a 4 exercícios finais

---
+++ M1-Fund.Des.Web/B4-Int.JavaScript.LogProgram/dia1/exerciciosFinais.js
@@ -0,0 +1,57 @@
+const a = 10
+const b = 8
+
+const soma = a+b
+console.log (soma)
+
+const subtrai = a-b
+console.log (subtrai)
+
+const multiplica = a*b
+console.log (multiplica)
+
+const divide = a/b
+console.log (divide)
+
+const resto = a%b
+console.log (resto)
+
+// ------------------------------------------------1
+
+const num1 = 3
+const num2 = 9
+
+if (num1 > num2) {
+    console.log (num1)
+}else {
+    console.log (num2)
+}
+
+// -------------------------------------------------2
+
+const ft1 = 99
+const ft2 = 55
+const ft3 = 22
+
+if (ft1 > ft2 && ft1 > ft3) {
+    console.log (ft1)
+} else if ( ft2 > ft1 && ft2 > ft3) {
+    console.log (ft2)
+} else {
+    console.log (ft3)
+}
+
+// --------------------------------------------------3
+
+const number =  99
+
+if (number < 0) {
+        console.log ("negative")
+} else if (number > 0) {
+        console.log ("positive")
+} else {
+    console.log ("zero")
+}
+
+// -------------------------------------------------4
+   
